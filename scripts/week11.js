62cd30d538398f762a8604c1add0b2c998e7673d
Toms-D-Barvidis <Toms-Deimonds.Barvidis@edu.rtu.lv>
2024-11-14 19:01:47 +0200

    js nonsense

--- scripts/week11.js
+++ scripts/week11.js
@@ -1,1 +1,45 @@
 console.log("Let's talk switches!")
+// lets see a switch statement in action.
+
+const today = "Thursday"
+let message = ""
+
+switch (today){
+    case "Monday":
+        console.log("Today is Monday");
+        message = "Today is Monday";
+        break;
+    case "Tuesday":
+        console.log("Today is Tuesday");
+        message = "Today is Tuesday";
+        break;
+    case "Wednesday":
+        console.log("Today is Wednesday");
+        message = "Today is Wednesday";
+        break;
+    case "Thursday":
+        console.log("Today is Thursday");
+        message = "Getting closer to Friday!";
+        break;
+    default:
+        console.log("This is wrong");
+        message = "Alien invasion";
+}
+
+const switchExample = document.getElementById("switch-example");
+switchExample.textContent = message;
+
+// WHILE LOOPS
+
+let i = 1;
+while (i <= 10){
+    console.log(i);
+    i++;
+}
+
+let j = 1;
+while (j <= 3){
+    const block = document.querySelector(".block-" + j);
+    block.textContent += " yabbadabba doo " + j;
+    j++;
+}
